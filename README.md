02cdcda3484c38a6c410c43b8538a32d610124e5
t170815518 <christang_1023@outlook.com>
2020-12-09 14:57:09 +0800

    ADD data preprocess

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # LAN-PyTorch
 Implementation of Logic Attention Network in PyTorch (currently without inductive test)
 
-**IMPORTANT Notes**:
+**Notes**:
 1. This repo currently does not support inductive testing, e.g. aux and dev data in the original repo are not 
 needed and removed. 
+1. This repo currently contains only Attention aggregator and TransE score. 
+1. L2 regularization is temporarily removed. 
 
 The model must be trained with GPU. 
+## Data requirement 
+Dataset should be put under `data` directory, and it should contain files including `train`, `test`, `entity2id.txt` and 
+`relation2id.txt`. To translate the data in index format, see `utils.preprocess` (the function will overwritten the original files!)
+## Evaluation Methods 
+Filter out the known entities (i.e., the ones in train set)
---
+++ utils/preprocess.py
@@ -0,0 +1,40 @@
+import os
+
+
+def preprocess(file_path):
+    entity2id = {}
+    rel2id = {}
+    entity_cnt = 0
+    rel_cnt = 0
+
+    for file_name in ["train.txt", "test.txt"]:
+        translated_data = []
+        with open(os.path.join(file_path, file_name), "r") as f:
+            for line in f:
+                e1, rel, e2 = line.strip().split("\t")
+                if e1 not in entity2id.keys():
+                    entity2id[e1] = entity_cnt
+                    entity_cnt += 1
+                if e2 not in entity2id.keys():
+                    entity2id[e2] = entity_cnt
+                    entity_cnt += 1
+                if rel not in rel2id.keys():
+                    rel2id[rel] = rel_cnt
+                    rel_cnt += 1
+                e1_id = entity2id[e1]
+                e2_id = entity2id[e2]
+                rel_id = rel2id[rel]
+                translated_data.append((e1_id, rel_id, e2_id))
+
+        with open(os.path.join(file_path, file_name), "w") as f:
+            for e1, rel, e2 in translated_data:
+                f.write("{}\t{}\t{}\n".format(e1, rel, e2))
+
+    for d, file_name in zip([entity2id, rel2id], ["entity2id.txt", "relation2id.txt"]):
+        with open(os.path.join(file_path, file_name), "w+") as f:
+            for name, index in d.items():
+                f.write("{}\t{}\n".format(name, index))
+
+
+if __name__ == '__main__':
+    preprocess("data/FB15k-237")
